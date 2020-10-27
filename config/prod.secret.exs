55866eaff70b396a993a7c64abe35c0cec84b4c2
Philip Chani <chaniphilip@hotmail.com>
2020-10-27 15:22:47 +0200

    Update prod.secret.exs

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -5,14 +5,10 @@
 use Mix.Config
 
 secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
-    raise """
-    environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
+  System.get_env("SECRET_KEY_BASE") || "< generate 32 characker key here with mix phx.gen.secret"
 
 config :scale_driver, ScaleDriverWeb.Endpoint,
-  http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
+  http: [port: String.to_integer(System.get_env("PORT") || "4321")],
   secret_key_base: secret_key_base
 
 # ## Using releases (Elixir v1.9+)
@@ -20,7 +16,7 @@ config :scale_driver, ScaleDriverWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :scale_driver, ScaleDriverWeb.Endpoint, server: true
+config :scale_driver, ScaleDriverWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
